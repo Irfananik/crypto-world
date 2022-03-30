cc1b5bf4e7c581f1bb6b758d6e898c6d0465d47d
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-03-30 23:52:25 +0600

    API load done

--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Coins = () => {
+    const [coins,setCoins] = useState()
+
+    useEffect(() => {
+        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false')
+        .then(response => response.json())
+        .then(data => setCoins(data))
+    },[])
     return (
         <div>
-            <h2>this is a coin section</h2>
+            <h2 className="text-3xl text-center font-sans font-bold mt-6">Crypto World Coins</h2>
+            <p className="text-xl text-center font-sans">Total Coins: {coins?.length}</p>
         </div>
     );
 };
