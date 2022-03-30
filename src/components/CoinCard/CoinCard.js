768ab838700ba375333978a94d04b04d5b4c516e
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-03-31 02:25:46 +0600

    all data show in card with UI design

---
+++ src/components/CoinCard/CoinCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CoinCard = ({ coin }) => {
+    return (
+        <div className="shadow-lg rounded-2xl bg-white p-4 w-[260px] mt-16">
+            <Link to='/coin'>
+                <div className="flex gap-4 justify-between items-center">
+                    <div className="flex-shrink-0">
+                        <img className="mx-auto object-cover rounded-full h-16 w-16" src={coin.image} alt="" />
+                    </div>
+                    <div className="flex flex-col justify-end">
+                        <span className="text-gray-500  font-medium">{coin.name}</span>
+                        <span className="text-gray-400  font-medium">{coin.symbol}</span>
+                    </div>
+                </div>
+            </Link>
+        </div>
+    );
+};
+
+export default CoinCard;
--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import CoinCard from '../CoinCard/CoinCard';
 
 const Coins = () => {
-    const [coins,setCoins] = useState()
+    const [coins, setCoins] = useState([])
 
     useEffect(() => {
         fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false')
-        .then(response => response.json())
-        .then(data => setCoins(data))
-    },[])
+            .then(response => response.json())
+            .then(data => setCoins(data))
+    }, [])
     return (
-        <div>
-            <h2 className="text-3xl text-center font-sans font-bold mt-6">Crypto World Coins</h2>
-            <p className="text-xl text-center font-sans">Total Coins: {coins?.length}</p>
+        <div className="px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2">
+            <h2 className="text-4xl text-center font-sans font-bold">Crypto World Coins</h2>
+            <p className="text-xl text-center font-sans text-gray-400">Available Amount: {coins?.length}</p>
+
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center">
+                {
+                    coins.map(coin => <CoinCard key={coin.id} coin={coin} />)
+                }
+            </div>
+
         </div>
     );
 };
