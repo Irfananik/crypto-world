8d67bd2293f6b341530446e6bfa3b343b269fcbd
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-03-31 13:26:23 +0600

    site complete

--- src/components/CoinDetails/CoinDetails.js
+++ src/components/CoinDetails/CoinDetails.js
@@ -1,41 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Spinner from '../Spinner/Spinner';
 
 const CoinDetails = () => {
     const { id } = useParams()
 
     const [coin, setCoins] = useState({})
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
         const url = `https://api.coingecko.com/api/v3/coins/${id}`
         fetch(url)
             .then(response => response.json())
             .then(data => setCoins(data))
+        setLoading(false)
     }, [id])
     return (
-        <div className="px-4 pt-20 pb-24 mx-auto max-w-7xl h-[90vh] md:px-2">
-            <div className="grid grid-cols-1 gap-4 h-full content-center justify-items-center md:grid-cols-2">
-                <div className="order-2 md:order-1">
-                    <h2 className="text-3xl">General Info:</h2>
-                    <p className="text-lg">Name: {coin.name} </p>
-                    <p className="text-lg">symbol: {coin.symbol} </p>
-                    <p className="text-lg">Market Rank: {coin.market_cap_rank} </p>
-                    <p className="text-lg">Origin: {coin.country_origin ? coin.country_origin : "Not available"} </p>
-                    <p className="text-lg">Contact Address: {coin.address} </p>
-                    <p className="text-lg">Current Priece: {coin.current_price} </p>
-                    <p className="text-lg">Last Updated: {coin.last_updated} </p>
+        <>
+            {loading ? <Spinner /> :
+                <div className="px-4 pt-20 pb-24 mx-auto max-w-7xl h-[90vh] md:px-2">
+                    <div className="grid grid-cols-1 gap-4 h-full content-center justify-items-center md:grid-cols-2">
+                        <div className="order-2 md:order-1">
+                            <h2 className="text-3xl">General Info:</h2>
+                            <p className="text-lg">Name: {coin.name} </p>
+                            <p className="text-lg">symbol: {coin.symbol} </p>
+                            <p className="text-lg">Market Rank: {coin.market_cap_rank} </p>
+                            <p className="text-lg">Origin: {coin.country_origin ? coin.country_origin : "Not available"} </p>
+                            <p className="text-lg">Contact Address: {coin.address} </p>
+                            <p className="text-lg">Current Priece: {coin.current_price} </p>
+                            <p className="text-lg">Last Updated: {coin.last_updated} </p>
 
-                    <h2 className="text-3xl">Score:</h2>
-                    <p className="text-lg">Community Score: {coin.community_score} </p>
-                    <p className="text-lg">Developer Score: {coin.developer_score} </p>
+                            <h2 className="text-3xl">Score:</h2>
+                            <p className="text-lg">Community Score: {coin.community_score} </p>
+                            <p className="text-lg">Developer Score: {coin.developer_score} </p>
+                        </div>
+                        <div className="flex justify-cneter items-center order-1 md:order-2">
+                            {
+                                <img src={coin.image?.large} alt="coinImg" />
+                            }
+                        </div>
+                    </div>
                 </div>
-                <div className="flex justify-cneter items-center order-1 md:order-2">
-                    {
-                        <img src={coin.image?.large} alt="coinImg" />
-                    }
-                </div>
-            </div>
-        </div>
+            }
+        </>
     );
 };
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className="sticky top-0 bg-white px-12 py-8 flex justify-center md:justify-between">
             <div className="hidden md:block">
-                <Link to='/' className="font-sans font-bold text-xl">Crypto World</Link>
+                <Link to='/' className="font-sans font-bold text-2xl">Crypto World</Link>
             </div>
             <div className="text-xl flex gap-4">
                 <CustomLink to='/'>Home</CustomLink>
