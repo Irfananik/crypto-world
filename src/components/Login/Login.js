be02d6d6fbce6743f0179ce092b5997c7f2c6ee2
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-04-08 23:18:21 +0600

    google authentication done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,25 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import app from '../firebase/firebase.init';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Login.css'
 
+const provider = new GoogleAuthProvider();
+
 const Login = () => {
     const navigate = useNavigate()
+
+    const googleAuth = () => {
+        const auth = getAuth(app);
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                const user = result.user
+                console.log(user)
+            }).catch((error) => {
+                const errorMessage = error.message
+                console.error(errorMessage)
+            });
+    }
     return (
         <div>
             <div className='auth-form-container '>
@@ -36,7 +52,7 @@ const Login = () => {
                         <div className='line-right' />
                     </div>
                     <div className='input-wrapper'>
-                        <button className='google-auth'>
+                        <button className='google-auth' onClick={googleAuth}>
                             <img src="" alt='' />
                             <p> Continue with Google </p>
                         </button>
